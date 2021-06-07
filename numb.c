2140da505fc0e3a28abfc95c44b51865be6dce0e
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2021-06-07 16:30:57 -0400

    Create numb.c

---
+++ numb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int numb;
+  srand(time(NULL);
+  numb = rand() % 5 + 1;
+  printf("> %d",numb);
+  return 0;
+        
+}
+  
